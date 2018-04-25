430f163840936afe49c300aa0678f5468ef6a534
balbini <balbini91@gmail.com>
2018-04-24 18:10:02 -0700

    added materialize, added admin value to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180424010652) do
+ActiveRecord::Schema.define(version: 20180425004435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20180424010652) do
     t.string "cardio_goals"
     t.string "password_digest"
     t.string "email"
+    t.boolean "admin"
   end
 
 end
