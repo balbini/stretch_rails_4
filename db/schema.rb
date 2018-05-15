62b2f92bbbc73b550fe613d139ed9948f6bf917e
balbini <balbini91@gmail.com>
2018-05-14 21:13:39 -0700

    small homepage changes, added user and workout relation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180429021709) do
+ActiveRecord::Schema.define(version: 20180515040954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,10 @@ ActiveRecord::Schema.define(version: 20180429021709) do
     t.string "experience"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_workouts_on_user_id"
   end
 
   add_foreign_key "blogs", "users"
+  add_foreign_key "workouts", "users"
 end
